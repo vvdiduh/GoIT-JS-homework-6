7224aebc4d544b9af31a336db7f33cc5fc988538
Viktor Didukh <vvdiduh@gmail.com>
2022-09-08 23:40:18 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngridientEl = document.querySelector("#ingredients")
+console.log(listIngridientEl);
+
+for (const ingredient of ingredients) {
+  // console.log(ingredient);
+  const listEl = document.createElement("li");
+  listIngridientEl.append(listEl);
+  listEl.classList.add("item");
+  listEl.textContent = ingredient;
+}
