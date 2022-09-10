3a2ff236dfe308a13eb897953eda25c8c2be1586
Viktor Didukh <vvdiduh@gmail.com>
2022-09-10 23:49:08 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#name-input');
+const nameEl = document.querySelector('#name-output');
+
+const handleInputName = (event) => {
+    // console.log(event.target.value);
+    nameEl.textContent = event.target.value;
+    if (nameEl.textContent === '') {
+        nameEl.textContent = 'Anonymous';
+    }
+}
+
+inputEl.addEventListener('input', handleInputName);
+
+// const name = (event) => {inputEl.textContent = event.target.}
