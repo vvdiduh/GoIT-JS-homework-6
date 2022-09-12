b86c1d5eeb9846b6c92938ec06e5d4692a2c5f12
Viktor Didukh <vvdiduh@gmail.com>
2022-09-12 16:53:54 +0300

    finish 08, 09 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector('.login-form');
+// console.log(formEl);
 
+const sendForm = (event) => {
+    event.preventDefault();
+    const { elements: { email, password }, } = event.target;
+
+    if (email.value === '') {
+        alert('введіть email');
+        return
+    } 
+
+    if (password.value === '') {
+        alert('введіть пароль');
+        return
+    } 
+
+    const userData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(userData);
+    console.log('форму відправило ');
+
+    formEl.reset();
+}
+
+formEl.addEventListener('submit', sendForm);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  bodyBgColor.style.backgroundColor =
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyBgColor = document.querySelector('body');
+const btnChangeBg = document.querySelector('.change-color');
+
+btnChangeBg.addEventListener ('click', getRandomHexColor)
+
